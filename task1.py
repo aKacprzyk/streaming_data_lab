b730548620b5d54b6e7f9a0699df194ed3636aeb
Aleksandra Kacprzyk <a.kcprzk@gmail.com>
2025-05-14 00:19:36 +0200

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import expr
+
+batch_counter = {"count": 0}
+
+def process_batch(df, batch_id):
+    batch_counter["count"] += 1
+    print(f"Batch ID: {batch_id}")
+    df.show(truncate=False)
+    if batch_counter["count"] % 7 == 0:
+        spark.stop()
+
+spark = SparkSession.builder.appName("StreamingDemo").getOrCreate()
+spark.sparkContext.setLogLevel("WARN")
+
+rate_df = (spark.readStream
+      .format("rate")
+      .option("rowsPerSecond", 5)
+      .load())
+ 
+events = (rate_df.withColumn("user_id", expr("concat('u', cast(rand()*100 as int))") )
+          .withColumn("event_type", expr("case when rand() > 0.7 then 'purchase' else 'view' end") )
+          .select("timestamp", "user_id", "event_type")
+         )
+    
+query = (events.writeStream
+         .format("console")
+         .foreachBatch(process_batch)
+         .start())
